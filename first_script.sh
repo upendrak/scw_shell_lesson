56588d5d9b93574a7b6b55a0dbdb1e69e9eb917d
upendrak <upendrakumar.devisetty@googlemail.com>
2017-02-25 12:29:15 -0700

    Add help function

--- first_script.sh
+++ first_script.sh
@@ -1,2 +1,7 @@
+if [ "$1" == "-h"] || [ "$1" == "--help"]; then
+   echo "Usage: sh $0 input_file output_file"
+   exit
+fi
+
 # This script is used to count the number of characters in a file  
 head -5 $1 | sort | wc -c > $2
